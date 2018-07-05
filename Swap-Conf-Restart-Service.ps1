32684e4fd48990579b3feda031588e0dbd33b28d
zuphzuph <zuphzuph@users.noreply.github.com>
2018-07-05 11:44:47 -0600

    Commited
    Done. o.O

---
+++ Swap-Conf-Restart-Service.ps1
@@ -0,0 +1,11 @@
+﻿$computers = Get-ADComputer -Filter {name -like "*"}
+$source = "path to ini or conf"
+$dest = "new location"
+foreach ($computer in $computers) {
+    if (test-Connection -Cn $computer -quiet) {
+        Copy-Item $source -Destination \\$computer\$dest -Force
+Get-Service -Name ServiceNameHere  -ComputerName $computers | Restart-service
+    } else {
+        "$computer is not online"
+           }
+	                              }
